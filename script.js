1a4b673719fe52f0b2a0114f479b7dbfc864e7b1
Fkayensu <frederickayensuphd@gmail.com>
2024-03-12 09:43:30 -0400

    add click to open and close modal feature

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Select objects for manipulation
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Define open modal functions
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// Define open modal functions
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Open modal upon clicking button
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// Close modal upon clicking close button or overlay
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
